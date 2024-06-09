1174328183c0fc8c43b8cf5e1be2d54aed188ae4
AKKKABUSSTTT <dev@akkkabussttt.xyz>
2024-06-09 23:57:18 +0300

    changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,17 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf_token" content="{{ csrf_token() }}">
+        <meta name="description" content="Bine ați venit în familia noastră! La noi veți găsi produse de sănătate ușor de utilizat, concepute pentru a vă sprijini în îmbunătățirea stilului de viață. Fiecare produs este selectat cu grijă pentru a oferi simplitate și eficiență, astfel încât să vă puteți concentra pe ceea ce contează cel mai mult: bunăstarea dumneavoastră. Suntem dedicați să creăm o comunitate unde fiecare membru se simte susținut și inspirat. Alăturați-vă nouă și descoperiți soluții inovatoare pentru o viață mai sănătoasă și mai fericită.">
+        <meta name="keywords" content="sanatate, apl, fericire, sanatosi, dureri, remedii, echipa, putere">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-953VZGK5CJ"></script>
         <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-        gtag('config', 'G-953VZGK5CJ');
+            gtag('config', 'G-953VZGK5CJ');
         </script>
 
         @routes
